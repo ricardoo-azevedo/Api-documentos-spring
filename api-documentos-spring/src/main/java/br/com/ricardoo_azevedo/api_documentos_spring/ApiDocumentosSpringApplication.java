e8b25deb8852d755f972ce110bd693a34f2829cf
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-13 00:47:37 -0300

    Inicialização do Projeto

---
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/ApiDocumentosSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricardoo_azevedo.api_documentos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocumentosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocumentosSpringApplication.class, args);
+	}
+
+}
---
+++ api-documentos-spring/src/test/java/br/com/ricardoo_azevedo/api_documentos_spring/ApiDocumentosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardoo_azevedo.api_documentos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocumentosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
