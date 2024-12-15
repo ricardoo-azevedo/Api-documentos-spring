5e6f626b52d59b17e5bf30fd3f70ae8eaac11dbb
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-15 14:20:38 -0300

    raw: inserção da anotação de Repository em etapaEnsinoRepository

--- api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/EtapaEnsinoRepository.java
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/EtapaEnsinoRepository.java
@@ -1,9 +1,11 @@
 package br.com.ricardoo_azevedo.api_documentos_spring.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.ricardoo_azevedo.api_documentos_spring.models.EtapaEnsino;
 
+@Repository
 public interface EtapaEnsinoRepository extends JpaRepository<EtapaEnsino, Long>{
 
     
