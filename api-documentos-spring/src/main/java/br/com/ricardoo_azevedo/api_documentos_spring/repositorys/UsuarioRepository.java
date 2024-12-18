380f50f16414fe26658d47f8d1ff5a8744b8517e
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-18 00:12:19 -0300

    feat: criação de alguns metodos importantes em UsuarioRepository

--- api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/UsuarioRepository.java
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package br.com.ricardoo_azevedo.api_documentos_spring.repositorys;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.ricardoo_azevedo.api_documentos_spring.models.Usuario;
@@ -12,5 +15,10 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     boolean existsByNomeCompleto(String nome);
 
+    boolean existsByEmail(String email);
+
     Optional<Usuario> findByNomeCompleto(String nome);
+
+    @Query(value = "SELECT * FROM TB_USUARIO WHERE nome_completo_usuario LIKE CONCAT('%', :nome, '%')", nativeQuery = true)
+    List<Usuario> findByNomeCompletoUsuario(@Param("nome") String nome);
 }
