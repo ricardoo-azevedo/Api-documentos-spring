0b76ca0f6371dc47c1a4ef0c04353c2c700d12e8
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-17 23:24:14 -0300

    feat: implementação de dois metodos para usuario

--- api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/UsuarioRepository.java
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/repositorys/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.ricardoo_azevedo.api_documentos_spring.repositorys;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import br.com.ricardoo_azevedo.api_documentos_spring.models.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-    
+    boolean existsByNomeCompleto(String nome);
+
+    Optional<Usuario> findByNomeCompleto(String nome);
 }
