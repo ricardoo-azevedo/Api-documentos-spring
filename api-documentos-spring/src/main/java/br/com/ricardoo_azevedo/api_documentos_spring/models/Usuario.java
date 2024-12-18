c814cdfb4a5376d883ca4e0150656b663d44fef4
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-17 21:49:44 -0300

    feat: criação do model e usuario

---
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/models/Usuario.java
@@ -0,0 +1,38 @@
+package br.com.ricardoo_azevedo.api_documentos_spring.models;
+
+import java.sql.Timestamp;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Component
+@Table(name = "tb_usuario")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Usuario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_completo_usuario", nullable = false, unique = true)
+    private String nomeCompleto;
+
+    @Column(name = "email_usuario", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha_usuario", nullable = false, unique = true)
+    private String senha;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp data_criacao;
+}
