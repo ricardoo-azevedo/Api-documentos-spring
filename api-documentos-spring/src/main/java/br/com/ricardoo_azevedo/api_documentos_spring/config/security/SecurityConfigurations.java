4ba85843dc0b6d4f7edbaf57ea585a5a98aa515d
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-12-20 18:58:40 -0300

    criação do metodo de filtro de autenticação

---
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/config/security/SecurityConfigurations.java
@@ -0,0 +1,26 @@
+package br.com.ricardoo_azevedo.api_documentos_spring.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(HttpMethod.POST, "/api/usuario").hasRole("Admin")
+        .anyRequest().authenticated()
+        )
+        .build();
+    }
+}
