683c8d9d417583f6fc69197399d2e4b006d21a77
ricardoo-azevedo <ricardoazevedo1911@gmail.com>
2025-01-23 04:29:54 -0300

    fix: colocando o input certo no controller de escola

--- api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/controller/EscolaController.java
+++ api-documentos-spring/src/main/java/br/com/ricardoo_azevedo/api_documentos_spring/controller/EscolaController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ricardoo_azevedo.api_documentos_spring.dtos.input.EscolaInputDto;
-import br.com.ricardoo_azevedo.api_documentos_spring.dtos.output.EscolaOutputDto;
+import br.com.ricardoo_azevedo.api_documentos_spring.dtos.EscolaDto;
 import br.com.ricardoo_azevedo.api_documentos_spring.services.impls.EscolaServiceImpl;
 import jakarta.validation.Valid;
 
@@ -28,49 +27,49 @@ public class EscolaController {
     private EscolaServiceImpl escolaServiceImpl;
 
     @PostMapping
-    public ResponseEntity<?> salvar(@RequestBody @Valid EscolaInputDto escolaDto, BindingResult bindingResult) {
+    public ResponseEntity<?> salvar(@RequestBody @Valid EscolaDto escolaDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
-        EscolaOutputDto escolaSalva = escolaServiceImpl.salvar(escolaDto);
+        EscolaDto escolaSalva = escolaServiceImpl.salvar(escolaDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(escolaSalva);
     }
 
     @PutMapping("/editar-por-id={id}")
-    public ResponseEntity<?> editarPorId(@RequestBody @Valid EscolaInputDto escolaDto,
+    public ResponseEntity<?> editarPorId(@RequestBody @Valid EscolaDto escolaDto,
             BindingResult bindingResult, @PathVariable Long id) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
-        EscolaOutputDto escolaEditada = escolaServiceImpl.editarPorId(escolaDto, id);
+        EscolaDto escolaEditada = escolaServiceImpl.editarPorId(escolaDto, id);
         return ResponseEntity.ok().body(escolaEditada);
     }
 
     @PutMapping("/editar-por-nome={nomeAntigo}")
-    public ResponseEntity<?> editarPorNome(@RequestBody @Valid EscolaInputDto escolaDto, BindingResult bindingResult,
+    public ResponseEntity<?> editarPorNome(@RequestBody @Valid EscolaDto escolaDto, BindingResult bindingResult,
             @PathVariable String nomeAntigo) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
         }
-        EscolaOutputDto escolaEditada = escolaServiceImpl.editarPorNome(escolaDto, nomeAntigo);
+        EscolaDto escolaEditada = escolaServiceImpl.editarPorNome(escolaDto, nomeAntigo);
         return ResponseEntity.ok().body(escolaEditada);
     }
 
     @GetMapping
     public ResponseEntity<List<?>> listar() {
-        List<EscolaOutputDto> escolaDtos = escolaServiceImpl.listar();
+        List<EscolaDto> escolaDtos = escolaServiceImpl.listar();
         return ResponseEntity.ok(escolaDtos);
     }
 
     @GetMapping("/pesquisar-por-id={id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
-        EscolaOutputDto escolaDto = escolaServiceImpl.pesquisarPorId(id);
+        EscolaDto escolaDto = escolaServiceImpl.pesquisarPorId(id);
         return ResponseEntity.ok().body(escolaDto);
     }
 
     @GetMapping("/pesquisar-por-nome={nome}")
     public ResponseEntity<List<?>> buscarPorNome(@PathVariable String nome) {
-        List<EscolaOutputDto> escolaDtos = escolaServiceImpl.pesquisarPorNome(nome);
+        List<EscolaDto> escolaDtos = escolaServiceImpl.pesquisarPorNome(nome);
         return ResponseEntity.ok(escolaDtos);
     }
 
